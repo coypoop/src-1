ca2dfe9973690e787622352e7eb51ab0882c310e
Maya Rashish <maya@NetBSD.org>
2019-07-04 16:48:49 +0300

    Fix struct drm_gem_cma_object. Now I can write to_drm_gem_cma_obj
    Add some function declarations.

--- sys/external/bsd/drm2/include/drm/drm_gem_cma_helper.h
+++ sys/external/bsd/drm2/include/drm/drm_gem_cma_helper.h
@@ -11,8 +11,24 @@ struct drm_gem_cma_object {
 	size_t dmasize;
 	void *vaddr;
 	bus_dmamap_t dmamap;
-	struct drm_gem_object *base;
+	struct drm_gem_object base;
 	struct sg_table *sgt;
 };
 
+struct sg_table * drm_gem_cma_prime_get_sg_table(struct drm_gem_object *);
+struct drm_gem_object * drm_gem_cma_prime_import_sg_table(struct drm_device *,
+    struct dma_buf_attachment *, struct sg_table *);
+void * drm_gem_cma_prime_vmap(struct drm_gem_object *);
+void drm_gem_cma_prime_vunmap(struct drm_gem_object *, void *);
+struct drm_gem_cma_object * drm_gem_cma_create(struct drm_device *, size_t);
+void drm_gem_cma_free_object(struct drm_gem_object *);
+
+static inline struct drm_gem_cma_object *
+to_drm_gem_cma_obj(struct drm_gem_object *gobj)
+{
+	return container_of(gobj, struct drm_gem_cma_object, base);
+}
+
+int drm_gem_cma_dumb_create(struct drm_file *, struct drm_device *,
+    struct drm_mode_create_dumb *);
 #endif
